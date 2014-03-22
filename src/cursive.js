ac311108a6868d828fcbc721c591e99d4d412958
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2014-03-22 12:50:43 +0100

    Build: 5 Branch: master  0.0.1 SHA: 63c6e2b

--- src/cursive.js
+++ src/cursive.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   return $(".screenshot").click(function(event) {
     event.preventDefault();
     $('.modal-body > img').attr('src', $(event.target).attr('src'));
-    return $('#myModal').modal();
+    return $('#screenshotModal').modal();
   });
 });
